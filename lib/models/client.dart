8e08a487700761d520ec1d7f9f6124c4e979d605
Daniel Vanko <daaniiieel@disroot.org>
2020-10-19 12:01:44 +0200

    initialize() fix, vscode config

--- lib/models/client.dart
+++ lib/models/client.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:lspclient/models/message.dart';
 import 'dart:math';
@@ -14,9 +15,11 @@ class LspClient {
   LspClient(
       {this.command, this.args, this.inputType, this.processId, this.logLevel});
 
-  Future<Process> initialize() async {
+  Future<bool> initialize() async {
+    var completer = Completer<bool>();
     var lspserver = await Process.start(command, args);
-    var id = Random().nextInt(10000);
+    var id = Random().nextInt(100000);
+
     var message = InitializeMessage(
             id: id,
             params: InitializeParams(
@@ -30,33 +33,44 @@ class LspClient {
                 },
                 trace: 'verbose'))
         .toString();
-
+    print('sending initialize with id $id');
     currentProcess = lspserver;
-    lspserver.stdin.write(BaseProtocol(
+    currentProcess.stdin.write(BaseProtocol(
             content: message,
             header: HeaderPart(contentLength: utf8.encode(message).length))
         .toMessage());
-    await for (var response in lspserver.stdout) {
-      var response_text = utf8.decode(response);
-      print(response_text);
-      var message;
-      try {
-        message =
-            json.decode(BaseProtocol.fromLSPMessage(response_text).content);
-      } catch (e) {
-        print('\nERROR: $e');
-        message =
-            json.decode(BaseProtocol.fromLSPMessage(response_text).content);
-      }
-      if (message['id'] == id) {
-        print('response appears to respond to respond to InitializeMessage');
+
+    var response_message = '';
+    var count = 0;
+    await for (var event in lspserver.stdout) {
+      if (!(count >= 2)) {
+        response_message += utf8.decode(event);
+        count++;
+        if (count >= 2) {
+          break;
+        }
       }
     }
-    return lspserver;
+
+    var base = BaseProtocol.fromLSPMessage(response_message);
+    if (json.decode(base.content)['id'] == id) {
+      print('Got initialize() response with id $id');
+      completer.complete(true);
+    }
+
+    return completer.future;
   }
 
   void shutdown() async {
     print('called shutdown()');
+    var message = ShutdownMessage(id: Random().nextInt(10000)).toString();
+    currentProcess.stdin.write(BaseProtocol(
+            header: HeaderPart(contentLength: utf8.encode(message).length))
+        .toMessage());
+  }
+
+  void exit() async {
+    print('called exit()');
     var message = ShutdownMessage(id: Random().nextInt(10000)).toString();
     currentProcess.stdin.write(BaseProtocol(
             header: HeaderPart(contentLength: utf8.encode(message).length))
--- lspclient.dart
+++ lspclient.dart
@@ -1,7 +1,6 @@
 import 'package:lspclient/models/client.dart';
 
 void main(List<String> arguments) async {
-  print('starting process');
   var client = LspClient(
       command: 'dart',
       args: [
